dc18c10ef1efd479dba31bf7711e752ce3ff0c2b
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-03 22:39:28 +0000

    a function that will return the tail of an array-everything except the first element

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀" + `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("🙁" + `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr){
+  let head = []
+  for(i = 0; i < arr.length; i++){
+    if(i !== 0){
+      head.push(arr[i])
+    }
+  }
+  
+  return head
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
