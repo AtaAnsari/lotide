25a611e491a3dce401daf3cb9963276987da4b94
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-04 22:29:47 +0000

    adding assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = function(arr1, arr2) {
+  for(i = 0; i < arr1.length; i++){
+    for(j = i; j <= i; j++){
+      if(arr1[i] !== arr2[j]){
+        return console.log("🙁" + `Assertion Failed: [${arr1}] !== [${arr2}]`)
+        
+      }
+    }
+  }
+  return console.log("😀" + `Assertion Passed: [${arr1}] === [${arr2}]`);
+};
+
