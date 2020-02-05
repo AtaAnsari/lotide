7f5f9f886ee311755945936470b1815d9b42b610
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-05 21:01:26 +0000

    completed solution

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀" + `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("🙁" + `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(showsTable, show) {
+  let showGenre;
+  let genreArray = Object.keys(showsTable);
+  for (let genre of genreArray) {
+    if (bestTVShowsByGenre[genre] === show) {
+      showGenre = genre;
+    }
+  }
+  console.log(showGenre);
+  return showGenre;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
