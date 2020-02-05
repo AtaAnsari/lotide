cacc30e92d0d7c6eac51cbd80e3a5a18f33f92e2
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-05 22:40:33 +0000

    works for primitives but not arrays

--- eqObjects.js
+++ eqObjects.js
@@ -16,10 +16,10 @@ const eqObjects = function(object1, object2) {
     for (let object1key of object1KeysArr) {
       for (let object2key of object2KeysArr) {
         if (object1key === object2key) {
-          foundMatch = true;
-        } else if (object1key === object2key) {
           if (object1[object1key] === object2[object2key]) {
             foundMatch = true;
+          } else {
+          return foundMatch = false;
           }
         }
       }
@@ -32,9 +32,17 @@ const eqObjects = function(object1, object2) {
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
+const cba = { c: "1", b: "2", a: "1" };
 const cab = { c: "3", a: "1", b: "2" };
 const xyz = { x: "3", y: "1", z: "2" };
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const ce = { c: "1", e: ["2", 3] };
 assertEqual(eqObjects(ab, ba), true);
 assertEqual(eqObjects(ab, abc), false);
 assertEqual(eqObjects(abc, cab), true);
+assertEqual(eqObjects(cba, cab), false);
 assertEqual(eqObjects(cab, xyz), false);
+
+// assertEqual(eqObjects(cd, dc), true);
+// assertEqual(eqObjects(ce, dc), false);
