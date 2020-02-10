461a991d4ff1bf65932b3722160bc698bb196193
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-10 15:08:51 +0000

    refactored tail function and created a tailTest file that utilizes module to access tail and assertEqual functions

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("😀" + `Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log("🙁" + `Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let head = [];
   for (let i = 0; i < arr.length; i++) {
@@ -17,8 +9,7 @@ const tail = function(arr) {
   return head;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail
+
+
 
---
+++ test/tailTest.js
@@ -0,0 +1,9 @@
+const tail = require('../tail');
+
+const assertEqual = require('../assertEqual');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
