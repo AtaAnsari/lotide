f302629e6504596a6d55014e745b69a520d59a12
AtaAnsari <ataulhaq.ansari@gmail.com>
2020-02-10 19:30:43 +0000

    refactored test files to utilize chai library and mocha framework

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,11 @@
-const headHunter = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const assertEqual = require('../assertEqual');
-
-assertEqual(headHunter([5,6,7]), 5);
-assertEqual(headHunter(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 5 for [5, 2, 3]", () => {
+    assert.strictEqual(head([5, 2, 3]), 5);
+  });
+});
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,15 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
 const middleIdentifier = require('../middle')
 
-assertArraysEqual(middleIdentifier([1,2,3,4,5,6,7]), [4])
+// assertArraysEqual(middleIdentifier([1,2,3,4,5,6,7]), [4])
 
-assertArraysEqual(middleIdentifier([1,2,3,4,5,6,7,8]), [4,5])
+// assertArraysEqual(middleIdentifier([1,2,3,4,5,6,7,8]), [4,5])
+
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("Returns the middle item(s)", () => {
+    const result = middleIdentifier([1,2,3,4,5,6,7]);
+    assert.deepEqual(result, [4])
+  })
+})
